2d2dcc053250deecb58d71649d0d0c26fbf9756d
lake <102535481@qq.com>
2017-04-07 10:53:29 +0800

    update

--- hook/check-hook.sh
+++ hook/check-hook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ps -ef | grep -v ".svn" | grep hook-index.js
-if [ $? -ne 0 ];
+ps -ef | grep -v ".svn" | grep -v "grep" | grep hook-index.js > /dev/null
+if [ $? -ne 0 ];then
 	nohup ./start.sh
 fi
